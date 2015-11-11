c6db44bdf7c65f980f517a0afaa5627413909d5a
Abhrojit Mukherjee <abhrojit.mukherjee1994@gmail.com>
2015-11-12 00:31:09 +0530

    Bug fixes for blank registration details in settings  Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/onclavesystems/cestemoeducare/SettingsFragment.java
+++ app/src/main/java/com/onclavesystems/cestemoeducare/SettingsFragment.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AlertDialog;
@@ -22,21 +23,39 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.activity_app_preference);
-        Preference pref = findPreference("registrationno");
+        final EditTextPreference pref = (EditTextPreference)findPreference("registrationno");
+
+        pref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                SharedPreferences sp = getActivity().getSharedPreferences("hiddenpref", Context.MODE_PRIVATE);
+                Boolean registered = sp.getBoolean("registered", false);
+                if (registered == false) {
+                    pref.setText("");
+                }
+
+                return true;
+            }
+        });
+
         pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
-                SharedPreferences appPrefs = getActivity().getSharedPreferences("com.onclavesystems.cestemoeducare_preferences", Context.MODE_PRIVATE);
-                SharedPreferences.Editor prefEd = appPrefs.edit();
-                prefEd.putString("name", "Abhrojit");
-                prefEd.commit();
+                if (!newValue.equals("")) {
+                    SharedPreferences appPrefs = getActivity().getSharedPreferences("com.onclavesystems.cestemoeducare_preferences", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor prefEd = appPrefs.edit();
+                    prefEd.putString("name", "Abhrojit");
+                    prefEd.commit();
 
-                SharedPreferences sp = getActivity().getSharedPreferences("hiddenpref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sp.edit();
-                editor.putBoolean("registered",true);
-                editor.commit();
+                    SharedPreferences sp = getActivity().getSharedPreferences("hiddenpref", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putBoolean("registered", true);
+                    editor.commit();
 
-                Toast.makeText(getActivity(), "Changed", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getActivity(), "Your registration number is saved", Toast.LENGTH_LONG).show();
+                } else {
+                    return false;
+                }
                 return true;
             }
         });
@@ -63,11 +82,9 @@ public class SettingsFragment extends PreferenceFragment {
                                 editor.putBoolean("registered", false);
                                 editor.commit();
 
-                                Toast.makeText(getActivity(), "Application will now restart", Toast.LENGTH_LONG).show();
+                                Toast.makeText(getActivity(), "Registration number removed successfully", Toast.LENGTH_LONG).show();
 
-                                Intent i = getActivity().getPackageManager().getLaunchIntentForPackage(getActivity().getPackageName());
-                                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(i);
+
                             }
                         }).create().show();
                 return true;
